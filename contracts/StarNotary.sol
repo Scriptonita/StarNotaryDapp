b289fee017846b5d114b12c34a2591c25a556aa5
Javi <contacto@scriptonita.com>
2020-04-11 19:57:42 +0200

    Task 1 Add a name and symbol properties

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -13,7 +13,9 @@ contract StarNotary is ERC721 {
 
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
+    string public constant name = "JHG Coin";
     // symbol: Is a short string like 'USD' -> 'American Dollar'
+    string public constant symbol = "JHG";
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
